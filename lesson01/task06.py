745fc111dfff95d109a1a6cd193d04924d4fee07
kv1nt <kv1nt.krm@gmail.com>
2021-06-12 14:31:02 +0300

    task_06 optimized

--- lesson01/task06.py
+++ lesson01/task06.py
@@ -1,9 +1,12 @@
-a = int(input('Кол-во км. в первый день пробежки: '))
-b = int(input('Желаемый результат в км.: '))
-day = 1
-result_km = a
-while result_km < b:
-    a = a * 0.1 + a
-    day += 1
-    result_km = result_km + a
-print(f'Достигните цели на: {day} день')
+while True:
+    start = float(input('Кол-во км. в первый день пробежки: '))
+    finish = float(input('Желаемый результат в км.: '))
+    day = 1
+    if start <= 0 or finish < 0:
+        print('Результаты должны быть больше нуля.')
+    else:
+        while start < finish:
+            start += start * 0.1
+            day += 1
+        print(f'Достигните цели на: {day} день')
+        break
