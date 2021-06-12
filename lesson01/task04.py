8fa99c48753150066dff71fe2dfee2064bb075c2
kv1nt <kv1nt.krm@gmail.com>
2021-06-12 14:01:22 +0300

    task_04 optimized

--- lesson01/task04.py
+++ lesson01/task04.py
@@ -3,9 +3,9 @@ maxn = number % 10
 while number > 0:
     number = number // 10
     if number % 10 > maxn:
-        maxn  = number % 10
+        maxn = number % 10
         if number > 9:
             continue
     else:
-        print ('Максимальное число:', maxn)
+        print('Максимальное число:', maxn)
         break
