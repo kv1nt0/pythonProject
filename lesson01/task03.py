20490c943ef10f83e62cfa6d250a16bb8fe72a7b
kv1nt <kv1nt.krm@gmail.com>
2021-06-12 15:32:01 +0300

    task_03 optimized

--- lesson01/task03.py
+++ lesson01/task03.py
@@ -1,3 +1,3 @@
-num = int(input ('Введите число:'))
+num = int(input('Введите число:'))
 answer = (num + int(str(num) + str(num)) + int(str(num) + str(num) + str(num)))
 print(f'Cумма чисел n + nn + nnn = {answer}')
